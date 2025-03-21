4079df7fa6a97245acc7c407e676d426f6f935c7
NotIrishRTX <192121509+NotIrishRTX@users.noreply.github.com>
2025-03-21 17:45:36 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -5,37 +5,30 @@ local identifiers = {
 }
 local isNotifyReady = false
 
--- Check if okokNotify is available
 Citizen.CreateThread(function()
     local resourceState = GetResourceState('okokNotify')
     isNotifyReady = (resourceState == "started" or resourceState == "starting")
     
-    -- Send initial request for identifiers
     Citizen.Wait(1000)
     TriggerServerEvent('primerp_vehwl:reloadwl')
 end)
 
--- Function to display notification
 function ShowNotification(message, notifType)
     notifType = notifType or 'error'
     
     if isNotifyReady then
-        -- Use okokNotify if available
         exports['okokNotify']:Alert("Vehicle Trust", message, 5000, notifType)
     else
-        -- Use native notification
         SetNotificationTextEntry('STRING')
         AddTextComponentString(message)
         DrawNotification(false, false)
     end
 end
 
--- Get current identifiers
 function getIdentifiers()
     return identifiers
 end
 
--- Main thread to check vehicle access periodically
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(10000)
@@ -43,12 +36,10 @@ Citizen.CreateThread(function()
     end
 end)
 
--- Initialize identifiers when player spawns
 AddEventHandler("playerSpawned", function()
     TriggerServerEvent("primerp_vehwl:reloadwl")
 end)
 
--- Store identifiers received from server
 RegisterNetEvent("primerp_vehwl:loadIdentifiers")
 AddEventHandler("primerp_vehwl:loadIdentifiers", function(ids)
     identifiers = ids
@@ -57,7 +48,6 @@ AddEventHandler("primerp_vehwl:loadIdentifiers", function(ids)
           ", License: " .. (identifiers.license or "none"))
 end)
 
--- Check if player can use vehicle
 RegisterNetEvent('primerp_vehwl:RunCode:Client')
 AddEventHandler('primerp_vehwl:RunCode:Client', function(allowedVehicles)
     local ped = PlayerPedId()
@@ -75,7 +65,6 @@ AddEventHandler('primerp_vehwl:RunCode:Client', function(allowedVehicles)
     local vehicleModel = GetEntityModel(veh)
     local modelName = GetDisplayNameFromVehicleModel(vehicleModel):lower()
     
-    -- Check if this is a whitelisted vehicle
     local isWhitelisted = false
     local hasAccess = false
     
@@ -84,7 +73,6 @@ AddEventHandler('primerp_vehwl:RunCode:Client', function(allowedVehicles)
         if GetHashKey(spawncode) == vehicleModel or spawncode:lower() == modelName then
             isWhitelisted = true
             
-            -- Check if any of the identifiers match
             if (identifiers.discord and identifiers.discord == vehicle.discord) or
                (identifiers.steam and identifiers.steam == vehicle.steam) or
                (identifiers.license and identifiers.license == vehicle.license) then
@@ -96,7 +84,6 @@ AddEventHandler('primerp_vehwl:RunCode:Client', function(allowedVehicles)
         end
     end
     
-    -- Kick player from vehicle if it's whitelisted but they don't have access
     if isWhitelisted and not hasAccess then
         TaskLeaveVehicle(ped, veh, 16)
         Citizen.Wait(1500)
@@ -105,13 +92,11 @@ AddEventHandler('primerp_vehwl:RunCode:Client', function(allowedVehicles)
     end
 end)
 
--- Command to reload whitelist
 RegisterCommand("reloadwl", function(source)
     TriggerServerEvent("primerp_vehwl:reloadwl")
     ShowNotification('~g~Vehicle trust system reloaded', 'info')
 end, false)
 
--- Command to check vehicle permissions
 RegisterCommand("vehaccess", function(source, args)
     local ped = PlayerPedId()
     if not IsPedInAnyVehicle(ped, false) then
@@ -126,7 +111,6 @@ RegisterCommand("vehaccess", function(source, args)
     TriggerServerEvent('primerp_vehwl:checkAccess', vehicleModel, modelName)
 end, false)
 
--- Event to display vehicle access status
 RegisterNetEvent('primerp_vehwl:accessStatus')
 AddEventHandler('primerp_vehwl:accessStatus', function(hasAccess, isWhitelisted, vehicleName)
     if isWhitelisted then
@@ -140,7 +124,6 @@ AddEventHandler('primerp_vehwl:accessStatus', function(hasAccess, isWhitelisted,
     end
 end)
 
--- List all vehicles player has access to
 RegisterCommand("myvehicles", function(source, args)
     TriggerServerEvent('primerp_vehwl:listMyVehicles')
 end, false)
@@ -164,6 +147,6 @@ AddEventHandler('primerp_vehwl:displayVehicleList', function(vehicles)
             template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(41, 41, 41, 0.6); border-radius: 3px;">{0}{1}</div>',
             args = {vehicle.spawncode, ownerText}
         })
-        Citizen.Wait(10) -- Small delay to prevent chat message overflow
+        Citizen.Wait(10)
     end
 end)
